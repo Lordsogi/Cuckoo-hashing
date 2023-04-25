62f4764761c9fb5d0477ef1609c7c712d78c52d4
Lordsogi <105051389+Lordsogi@users.noreply.github.com>
2023-04-25 06:25:21 -0400

    Add files via upload

---
+++ CuckooMap.java
@@ -0,0 +1,105 @@
+package com.mycompany.app;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * CukooMap
+ */
+public class CuckooMap<K, V> implements Map<K, V> {
+
+    public CuckooMap(int numberOfBuckets) {
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return true;
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return false;
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return false;
+    }
+
+    @Override
+    public V get(Object key) {
+        return null;
+    }
+
+    public MapEntry getEntry(int index) {
+        return null;
+    }
+
+    @Override
+    public V put(K key, V value) {
+        return null;
+    }
+
+    @Override
+    public V remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+    }
+
+    @Override
+    public void clear() {
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return null;
+    }
+
+    @Override
+    public Collection<V> values() {
+        return null;
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return null;
+    }
+
+    public class MapEntry implements Entry<K, V> {
+        private K key;
+        private V value;
+
+        public MapEntry(K someKey, V someValue) {
+            this.key = someKey;
+            this.value = someValue;
+        }
+
+        @Override
+        public K getKey() {
+            return key;
+        }
+
+        @Override
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public V setValue(V newValue) {
+            V oldValue = value;
+            value = newValue;
+            return oldValue;
+        }
+
+    }
+
+}
