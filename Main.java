c00f8e7077a331d649dcced0833f982413b79e6e
Lordsogi <105051389+Lordsogi@users.noreply.github.com>
2023-05-17 12:54:50 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+package com.mycompany.app;
+
 public class Main {
     public static void main(String[] args) {
+        CuckooMap<Integer, String> test3 = new CuckooMap<>(34);
 
+        test3.put(26, "test test");
+        System.out.print(test3.get(26));
     }
 }
