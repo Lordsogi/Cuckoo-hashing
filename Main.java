af5610e0da94f6667245cad0dd8dfd686b9349ef
Lordsogi <105051389+Lordsogi@users.noreply.github.com>
2023-05-23 09:41:48 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,9 +2,84 @@ package com.mycompany.app;
 
 public class Main {
     public static void main(String[] args) {
-        CuckooMap<Integer, String> test3 = new CuckooMap<>(34);
+        
+        /*
+        CuckooMap<Integer, String> test = new CuckooMap<>(3);
 
-        test3.put(26, "test test");
-        System.out.print(test3.get(26));
+        test.put(0, "hello");
+        test.put(1, "hello");
+        test.put(2, "hello");
+        test.put(3, "hello");
+        test.put(4, "hello");
+
+        for (int i = 0; i < test.size(); i++) {
+            System.out.println(test.get(i));
+        }
+
+        System.out.println(test.containsKey(0));
+
+        CuckooMap<Integer, String> test2 = new CuckooMap<>(3);
+
+        test2.putAll(test);
+
+        for (int i = 0; i < test2.size(); i++) {
+            System.out.println(test2.get(i));
+        }
+        */
+        /*
+        System.out.println("Empty? " + test.isEmpty());
+
+        test.put(26, "test test");
+        System.out.println(test.get(26));
+
+        test.put(33, "test 2");
+        System.out.println(test.get(33));
+
+        test.put(42, "42");
+        System.out.println(test.get(42));
+
+        System.out.println("Size is " + test.size());
+
+        test.remove(42);
+        System.out.println(test.get(42));
+
+        boolean result = test.containsValue("test test");
+        System.out.println(result);
+
+        test.remove(26);
+        result = test.containsValue("test test");
+        System.out.println(result);
+
+
+        System.out.println("Empty? " + test.isEmpty());
+
+        boolean result2 = test.containsKey(33); ////
+        System.out.println(result2);////////////////
+
+
+        System.out.println("Size is " + test.size());
+
+        test.clear();
+
+        System.out.println("Empty? " + test.isEmpty());
+
+        CuckooMap<Integer, String> test2 = new CuckooMap<>(4);
+
+        test2.put(1, "some value");
+        test2.put(1, "another value");
+
+        CuckooMap<Integer, String> test3 = new CuckooMap<>(8);
+
+        System.out.println(test2.size());
+
+        test3.putAll(test2);
+
+        for (int i = 0; i < test3.size(); i++){
+            System.out.println(test3.get(i));
+        }
+         */
+
+
+
     }
 }
